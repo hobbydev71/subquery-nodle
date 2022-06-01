59e5f463d48f76895b2cdf32dc72a4076a88b37a
hobbydev71 <kamhongro@gmail.com>
2022-06-01 16:36:10 -0700

    update vesting

--- dist/mappings/mappingHandlers.js
+++ dist/mappings/mappingHandlers.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.handleCall = exports.handleEvent = exports.handleBlock = void 0;
 const block_1 = require("../handlers/block");
 const handlers_1 = require("../handlers");
+const vestingschedule_1 = require("../handlers/vestingschedule");
 async function handleBlock(block) {
     const handler = new block_1.BlockHandler(block);
     await handler.save();
@@ -11,6 +12,10 @@ exports.handleBlock = handleBlock;
 async function handleEvent(event) {
     const handler = new handlers_1.EventHandler(event);
     await handler.save();
+    if (event.event.section === "vesting" && event.event.method === "VestingScheduleAdded") {
+        const handler = new vestingschedule_1.VestingScheduleHandler(event);
+        await handler.save();
+    }
 }
 exports.handleEvent = handleEvent;
 async function handleCall(extrinsic) {
--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,8 +1,9 @@
 import { SubstrateBlock, SubstrateExtrinsic, SubstrateEvent } from '@subql/types'
-
+import { VESTING, ADD_VESTING_SCHEDULE } from '../handlers/types'
 import { BlockHandler } from '../handlers/block'
 import { EventHandler, ExtrinsicHandler } from '../handlers'
 import { CallHandler } from '../handlers/call'
+import { VestingScheduleHandler } from '../handlers/vestingschedule'
 
 export async function handleBlock(block: SubstrateBlock): Promise<void> {
     const handler = new BlockHandler(block)
@@ -14,6 +15,12 @@ export async function handleEvent(event: SubstrateEvent): Promise<void> {
     const handler = new EventHandler(event)
 
     await handler.save()
+
+    if( event.event.section === "vesting" && event.event.method === "VestingScheduleAdded") {
+        const handler = new VestingScheduleHandler(event)
+        
+        await handler.save()
+    }
 }
 
 export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
